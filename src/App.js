ca76cb57e32fc2ea7a96dc9820eac18b63df87d1
APPS4444 <aparnajitripathi@gmail.com>
2020-12-28 08:57:00 +0530

    deploy my react-app to github-pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello mom how r u</h1>
+      <h2>aparna i am fine</h2>
+      <h3>parth what r u doing</h3>
+    </div>
+  );
+}
+
+export default App;
